eb173ed80869e0980d0de0a924e74201bd41161b
lkitching <lee.kitching@gmail.com>
2014-07-03 19:46:41 +0100

    Add documentation for bounded and add some bounded instances.

--- FunSharp/Typeclasses/Bounded.cs
+++ FunSharp/Typeclasses/Bounded.cs
@@ -1,32 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using FunSharp.Ord;
 
 namespace FunSharp.Typeclasses
 {
+    /// <summary>Represents a type with a minimum and maximum value.</summary>
+    /// <typeparam name="T">The bounded type.</typeparam>
     public interface IBounded<T>
     {
+        /// <summary>Gets the minimum value for <typeparam name="T"/>.</summary>
         T MinBound { get; }
+
+        /// <summary>Gets the maximum value for <typeparam name="T"/>.</summary>
         T MaxBound { get; }
     }
 
+    /// <summary>Implementation of <see cref="IBounded{T}"/>.</summary>
+    /// <typeparam name="T">The  bounded type.</typeparam>
     public class Bounded<T> : IBounded<T>
     {
+        /// <summary>Creates a new instance of this class.</summary>
+        /// <param name="min">The minimum value of this type.</param>
+        /// <param name="max">The maximum value of this type.</param>
         public Bounded(T min, T max)
         {
             this.MinBound = min;
             this.MaxBound = max;
         }
 
+        /// <summary>Gets the minimum value.</summary>
         public T MinBound { get; private set; }
+
+        /// <summary>Gets the maximum value.</summary>
         public T MaxBound { get; private set; }
     }
 
     public static class BoundedInstances
     {
+        public static readonly IBounded<bool> Bool = new Bounded<bool>(false, true);
         public static readonly IBounded<sbyte> SByte = new Bounded<sbyte>(sbyte.MinValue, sbyte.MaxValue);
         public static readonly IBounded<byte> Byte = new Bounded<byte>(byte.MinValue, byte.MaxValue);
         public static readonly IBounded<char> Char = new Bounded<char>(char.MinValue, char.MaxValue);
@@ -40,5 +50,7 @@ namespace FunSharp.Typeclasses
         public static readonly IBounded<double> Double = new Bounded<double>(double.MinValue, double.MaxValue);
         public static readonly IBounded<decimal> Decimal = new Bounded<decimal>(decimal.MinValue, decimal.MaxValue);
         public static readonly IBounded<DateTime> DateTime = new Bounded<DateTime>(System.DateTime.MinValue, System.DateTime.MaxValue);
+        public static readonly IBounded<Unit> Unit = new Bounded<Unit>(FunSharp.Unit.Instance, FunSharp.Unit.Instance);
+        public static readonly IBounded<Ordering> Ordering = new Bounded<Ordering>(Ord.Ordering.LT, Ord.Ordering.GT);
     }
 }
